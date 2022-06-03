847dddfa37524f47bf7911dfad26dfaa6d5cba25
0MiniPorrinha <hugorodrigues969@gmail.com>
2022-06-03 15:27:17 -0300

    OK

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,37 @@
 package application;
 
+import model.entities.Account;
+import model.exceptions.WithdrawException;
+
+import java.util.Scanner;
+
 public class Program {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Scanner sc = new Scanner(System.in);
+
+        try {
+
+            System.out.println("Enter account data");
+            System.out.print("Number: ");
+            int number = sc.nextInt();
+            System.out.print("Holder: ");
+            sc.nextLine();
+            String holder = sc.nextLine();
+            System.out.print("Initial balance:");
+            double initialBalance = sc.nextDouble();
+            System.out.print("Withdraw limit: ");
+            double withdrawLimit = sc.nextDouble();
+
+            Account account = new Account(number, holder, initialBalance, withdrawLimit);
+
+            System.out.print("\nEnter amount for withdraw: ");
+            double withdraw = sc.nextDouble();
+            account.withdraw(withdraw);
+            System.out.println("New balance: " + account.getBalance());
+        }catch (WithdrawException e){
+            System.out.println("Withdraw error: " + e.getMessage());
+        }
+
+        sc.close();
     }
 }
